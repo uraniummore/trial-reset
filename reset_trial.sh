54b02e1e12f07133adadede605d8a3725856d53b
Sulik <uraniummore@gmail.com>
2022-05-29 17:22:13 +0600

    Initial commit.

---
+++ reset_trial.sh
@@ -0,0 +1,6 @@
+rm -rf ~/.config/JetBrains/PhpStorm2021.1/eval
+rm ~/.config/JetBrains/PhpStorm2021.1/options/other.xml
+rm -rf ~/.config/JetBrains/PyCharm2021.1/eval
+rm ~/.config/JetBrains/PyCharm2021.1/options/other.xml
+rm -rf ~/.local/share/JetBrains/consentOptions
+rm -rf ~/.java/.userPrefs
